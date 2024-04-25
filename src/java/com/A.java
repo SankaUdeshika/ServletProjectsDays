b30b055266659062480b26f34eed0249f33ff4ed
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-04-26 01:56:20 +0530

    WCD Day 11 (Session & Cookies)

--- src/java/com/A.java
+++ src/java/com/A.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "A", value = "/A")
@@ -13,17 +14,17 @@ public class A extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resposne) throws ServletException, IOException {
-        Cookie c = new Cookie("name", "WCD");
 
-//        c.setAttribute("Path", "/app");
-//        c.setValue("ChangedValue");
-//        c.setPath(getServletContext().getContextPath() + "/login");
-//        c.setPath("/WebText/login");
-//        c.setDomain("google.com"); // set Domain name. reject wenawa thiyena DOmain ekath ekka wenas wena nisa
-//        c.setMaxAge(10); // expire Time (-1 thibboth save wenne na) (0 imidiate Remove wenawa) (1< dammoth eka e time wenakn thiyenawa)
-//        c.setHttpOnly(true); // http walata witharai wada karanne
-        c.setSecure(true); // https walata wtharak da naththan http walata witharak da wada karanne kiyala danna puluwan
+        HttpSession session = request.getSession(); // This is the way How To create a Session ID
+        session.setAttribute("name", "HadapuSessionEka");
+//        System.out.println(session.getId());
+
+        Cookie c = new Cookie("JSESSIONID", "HadapuCookieeka");
+        c.setMaxAge(3600);
+        c.setHttpOnly(true);
+        c.setPath(getServletContext().getContextPath());
 
         resposne.addCookie(c);
+        System.out.println(c);
     }
 }
